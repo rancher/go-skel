0d1c4b0b15b070cce70be4b88342eb285f8293d5
Ivan Mikushin <i.mikushin@gmail.com>
2016-07-12 13:23:39 -0700

    Multi-arch build

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Sirupsen/logrus"
+	"github.com/urfave/cli"
 )
 
+var VERSION = "v0.0.0-dev"
+
 func main() {
-	logrus.Info("I'm a turkey")
+	app := cli.NewApp()
+	app.Name = "%APP%"
+	app.Version = VERSION
+	app.Usage = "You need help!"
+	app.Action = func(c *cli.Context) error {
+		logrus.Info("I'm a turkey")
+		return nil
+	}
+
+	app.Run(os.Args)
 }
