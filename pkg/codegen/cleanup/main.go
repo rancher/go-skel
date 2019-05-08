1218a9e9d6b2f359a0d7b3359aa9003151640ab5
Luther Monson <luther.monson@gmail.com>
2019-05-08 11:55:29 -0700

    removed klog for logrus, added urfave/cli back, removed go.sum

--- pkg/codegen/cleanup/main.go
+++ pkg/codegen/cleanup/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/rancher/wrangler/pkg/cleanup"
-	"k8s.io/klog"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	if err := cleanup.Cleanup("./pkg/apis"); err != nil {
-		klog.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
